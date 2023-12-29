9ce8a62ab1554642e41c97436d7bf47815376288
root <sakurakyuo@protonmail.com>
2023-12-29 16:40:12 +0000

    sm8450-common: drop some pixelworks

--- common.mk
+++ common.mk
@@ -126,13 +126,15 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     init.qti.display_boot.rc \
     init.qti.display_boot.sh \
-    gralloc.default \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
+    libgralloc.qti \
     libqdMetaData \
     libqdMetaData.system \
+    libqdutils \
     libsdmcore \
     libsdmutils \
+    libtinyxml \
     vendor.display.config@1.11.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
